54bbbc63f6780c2476b8cda824ff65e6409901b7
Takahata Shinji <takahata.shinji@elephantech.co.jp>
2021-10-25 13:51:28 +0900

    イニシャルコミット

---
+++ _Assets/MakeTitle.js
@@ -0,0 +1,28 @@
+﻿//
+// タイトル作成
+// ファイル名("####.html")からタイトル表示("####")を作成する
+//
+
+document.addEventListener('DOMContentLoaded', function ()
+{
+  var contentsList  = document.getElementById('title');  // 格納先ID
+  var loc           = document.createElement('h1');    // 格納先タグ
+
+  // ファイル名取得 ("####.html")
+  // パーセントエンコーディング解除
+  var fileName = window.location.href.split('/').pop();
+  fileName = decodeURI(fileName);
+  
+  // タイトル取得 ("####")
+  var title = fileName.split('.html')[0];
+  
+  // デコードしきれない特殊文字を変換
+  title = title.replaceAll('%23', '#');		//< #
+
+  // ドキュメントタイトル設定
+  document.title = title;
+  loc.innerHTML = title;
+
+  // 画面に反映
+  contentsList.appendChild(loc);
+});
---
+++ _Assets/MakeToc_L.js
@@ -0,0 +1,99 @@
+//
+// 目次(Table Of Contents)作成
+// 文書中の H2 、H3 、H4 タグをスキャンし、目次化する
+// (見出しをリンク化)
+//
+
+// 2021.10.20 各見出しへのリンクが正常に機能しないため使用不可
+
+document.addEventListener('DOMContentLoaded', function ()
+{
+  var contentsList = document.getElementById('toc'); // 目次を追加する先(table of contents)
+  var div = document.createElement('div');// 作成する目次のコンテナ要素
+
+  // h2、h3、h4 要素を全て取得する
+  var matches = document.querySelectorAll('h2, h3, h4');
+
+  // 取得した見出しタグ要素の数だけ以下の操作を繰り返す
+  matches.forEach(function (value, i)
+  {
+    // 見出しタグ要素のidを取得し空の場合は内容をidにする
+    var id = value.id;
+    if(id === '')
+    {
+      id = value.textContent;
+      value.id = id;
+    }
+
+    // 要素が h2 タグの場合
+    if( value.tagName === 'H2' )
+    {
+      var ul = document.createElement('ul');
+      var li = document.createElement('li');
+      var a = document.createElement('a');
+
+      // ul クラス指定
+      ul.className = "lv1";
+
+      // 追加する<ul><li><a>タイトル</a></li></ul>を準備する
+      a.innerHTML = value.textContent;
+      a.href = '#' + value.id;
+      li.appendChild(a)
+      ul.appendChild(li);
+
+      // コンテナ要素である<div>の中に要素を追加する
+      div.appendChild(ul);
+    }
+
+    // 要素が h3 タグの場合
+    if( value.tagName === 'H3' )
+    {
+      var ul = document.createElement('ul');
+      var li = document.createElement('li');
+      var a = document.createElement('a');
+      
+      // ul クラス指定
+      ul.className = "lv2";
+
+      // コンテナ要素である<div>の中から最後の<li>を取得する。
+      var lastUl = div.lastElementChild;
+      var lastLi = lastUl.lastElementChild;
+
+      // 追加する<ul><li><a>タイトル</a></li></ul>を準備する
+      a.innerHTML = value.textContent;
+      a.href = '#' + value.id;
+      li.appendChild(a)
+      ul.appendChild(li);
+
+      // 最後の<Ul>の中に要素を追加する
+      lastUl.appendChild(ul);
+    }
+
+    // 要素が h4 タグの場合
+    if( value.tagName === 'H4' )
+    {
+      var ul = document.createElement('ul');
+      var li = document.createElement('li');
+      var a = document.createElement('a');
+      
+      // ul クラス指定
+      ul.className = "lv3";
+
+      // コンテナ要素である<div>の中から最後の<li>を取得する。
+      var lastUl = div.lastElementChild;
+      var lastLi = lastUl.lastElementChild;
+
+      // 追加する<ul><li><a>タイトル</a></li></ul>を準備する
+      a.innerHTML = value.textContent;
+      a.href = '#' + value.id;
+      li.appendChild(a)
+      ul.appendChild(li);
+
+      // 最後の<li>の中に要素を追加する
+      lastLi.appendChild(ul);
+    }
+  });
+
+  // 最後に画面にレンダリング
+  contentsList.appendChild(div);
+});
